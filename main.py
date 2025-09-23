47c759dceeba4d9875c0ab0e8a8538e50c7e77cd
cerake <107406386+cerakesss@users.noreply.github.com>
2025-09-23 11:40:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import random
 
+def who_win(userhod, bothod):
+    if userhod == bothod:
+        print(f'Tie! Bot threw {bothod}')
+    # if you threw rock the program checks: if you threw rock and bot threw scissors you win, if you threw rock and bot threw paper you lose, and so on below
+    elif (
+        (userhod == 'Rock' and bothod == 'Scissors') or
+        (userhod == 'Scissors' and bothod == 'Paper') or
+        (userhod == 'Paper' and bothod == 'Rock')
+    ):
+        print(f'You won! Bot chose {bothod}')
+    else:
+        print(f'You lost! Bot chose {bothod}')
+
+def bot_hod_gen(botrand):
+    # if bot chose 1 then it threw rock
+    if botrand == 1:
+        rps2 = 'Rock'
+    # if bot chose 2 then it threw scissors
+    elif botrand == 2:
+        rps2 = "Scissors"
+    # if none of the above happened then bot threw paper
+    else:
+        rps2 = 'Paper'
+    return rps2
+
 
 # game loop
 while True:
-    # generate bot's choice
+    # generate what the bot will throw
     rps = random.randint(1, 3)
     
-    # get user's choice
-    rps3 = input('Choose what you want to pick, rock, scissors or paper: ')
+    # what the user will throw
+    rps3 = input('Choose what you want to pick, rock, scissors, paper: ').capitalize()
 
-    # if bot chose 1 then it's rock
-    if rps == 1:
-        rps2 = 'Rock'
-    # if bot chose 2 then it's scissors
-    elif rps == 2:
-        rps2 = "Scissors"
-    # if none of the above happened then bot chose paper
-    else:
-        rps2 = 'Paper'
+    commands_list = ['quit', 'q','exit']
+    if rps3 in commands_list:
+        print('Shutting down!')
+        break
 
-    # if your choice and bot's choice are the same then it's a tie
-    if rps3 == rps2:
-        print(f'Tie! Bot threw {rps2}')
-    # if you threw rock the program checks: if you threw rock and bot threw scissors you win, if you threw rock and bot threw paper you lose, and so on below
-    elif (
-        (rps3 == 'Rock' and rps2 == 'Scissors') or
-        (rps3 == 'Scissors' and rps2 == 'Paper') or
-        (rps3 == 'Paper' and rps2 == 'Rock')
-    ):
-        print(f'You won! Bot chose {rps2}')
-    else:
-        print(f'You lost! Bot chose {rps2}')
+    elif rps3 not in ['Rock', 'Scissors', 'Paper']:
+        print('Error! Enter one of the allowed values.')
+        continue
+
+    rps2 = bot_hod_gen(rps)
+
+    who_win(rps3, rps2)
